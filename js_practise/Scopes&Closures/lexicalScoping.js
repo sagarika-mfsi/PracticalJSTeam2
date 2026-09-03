ae16b01f4cefcbafe7240e92a2c815ab4a8438c3
sagarika <sagarikas@mindfiresolutions.com>
2026-09-03 11:39:19 +0530

    add lexical scoping example to demonstrate variable accessibility in nested functions

---
+++ js_practise/Scopes&Closures/lexicalScoping.js
@@ -0,0 +1,17 @@
+function p1(){
+    let num = 10; //accessible by both childs i.e s1, s2
+    console.log(str1); //ReferenceError: str1 is not defined even parent cant access the child var
+    
+    function s1(){
+    let str1 = "hey from s1"; //only accessible inside s1 where it is defined(same for s2)
+    console.log(num); //10
+    }
+    
+    function s2(){
+        console.log(num);//10  
+        console.log(str1); //ReferenceError: str1 is not defined
+    }
+    s1();
+    s2();
+}
+p1();
