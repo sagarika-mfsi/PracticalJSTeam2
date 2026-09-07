51e9482bb5bad0c78ee57b29d5f084d1df108700
sagarika <sagarikas@mindfiresolutions.com>
2026-09-07 09:32:38 +0530

    add promise examples demonstrating resolve and reject functionality

---
+++ js_practise/Promise/promise.js
@@ -0,0 +1,42 @@
+let promise = new Promise((resolve, reject) => {
+    let loginSuccess = true;
+    if (loginSuccess) {
+        resolve("Login successful");
+    } else {
+        reject("Login failed");
+    }
+});
+
+console.log(promise); //here promise is an  object of Promise { 'Login successful' }
+//to acess the value we use then
+
+//Promise with .then(), .catch(), .finally()
+let promise2 = new Promise(
+    (resolve, reject) => {
+        let flag = false;
+        if (flag) {
+            resolve("promise resolved!!");
+        }
+        else {
+            reject("promise rejected!!");
+        }
+    }
+)
+promise2.then((val) => { console.log(val) }).catch((val) => { console.log(val) }).finally(() => console.log("END!!"));
+
+
+//Promise.resolve()
+let p = Promise.resolve("Hello");
+
+p.then(result => {
+    console.log(result);
+});
+
+
+//Promise.reject()
+let p2 = Promise.reject("Something went wrong");
+
+p2.catch(error => {
+    console.log(error);
+}); //Something went wrong
+
