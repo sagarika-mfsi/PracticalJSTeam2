74af7706c677ed3c5da8c4e7ea7d5f4135aa7a2a
sagarika <sagarikas@mindfiresolutions.com>
2026-09-07 20:48:14 +0530

    add examples for promise chaining, Promise.all, and Promise.race in promise.js

--- js_practise/Promise/promise.js
+++ js_practise/Promise/promise.js
@@ -45,3 +45,108 @@ p2.catch(error => {
     console.log(error);
 }); //Something went wrong
 
+
+//Promise chaining------------------
+//------------------------------------
+function f1() { //here f1 function which returns one promise having one success value 10
+    return new Promise(
+        (resolve) => {
+            resolve(10);
+        }
+    )
+}
+//console.log(f1());
+f1()
+    .then( //1st value i.e resolve value from top i.e 10
+        (val) => {
+            console.log("1st got ", val);
+            return val * 2; //10 got multiplied i.e 20
+        }
+    )
+    .then(
+        (val2) => { // 20 came from 1st then
+            console.log("2nd got ", val2);
+            return val2 + 2; // 20 got added by 2 and sent to 3rd then
+        }
+    )
+    .then( //here there is no next then so it will not chained further
+        (val3) => {
+            console.log("finally got ", val3);
+        }
+    )
+    .catch( // if any error got occured then will caught by this catch
+        (err) => console.log(err)
+    )
+    .finally( // as always it got executed
+        () => {
+            console.log("ended!!");
+        }
+    )
+
+//Promise.all() :
+//--------------
+//waits for all Promises to succeed. If even one Promise rejects, the entire Promise.all() rejects.
+
+let prom1 = Promise.resolve("Apple");
+
+let prom2 = Promise.reject("Banana failed");
+
+let prom3 = Promise.resolve("Mango");
+
+try {
+    let result = await Promise.all([prom1, prom2, prom3]);
+    console.log(result);
+
+} catch (error) {
+    console.log(error);
+}
+
+/*
+Promise.all()
+
+ALL must succeed
+       ↓
+   Otherwise
+       ↓
+    REJECT*/
+
+//Promise.race():
+//------------------
+//"Whichever Promise finishes first, give me its result." It doesn't care whether the first Promise is successful or failed.
+/*
+Promise.race()
+      ↓
+FIRST TO FINISH
+      ↓
+success OR failure*/
+let myProm1 = new Promise(
+    (reject) => {
+        setTimeout(
+            () => {
+                reject("myProm1 is rejected!!")
+            }, 1000
+        )
+    }
+)
+let myProm2 = new Promise(
+    (resolve) => {
+        setTimeout(
+            () => {
+                resolve("myProm2 is resolved!!")
+            }, 2000
+        )
+    }
+)
+
+try {
+    let result = await Promise.race([myProm1, myProm2]);
+    console.log(result); //myProm1 will got 1st as it got settled 1st
+}
+catch (err) {
+    console.log(err);
+}
+
+
+/*
+race() → first one to finish wins either success or failure
+any() → first one to succeed wins */
