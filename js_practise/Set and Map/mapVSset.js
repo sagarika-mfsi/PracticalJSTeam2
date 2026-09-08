d310d0615f5895c41358f85ae894c83898e600ae
sagarika <sagarikas@mindfiresolutions.com>
2026-09-09 00:01:50 +0530

    add mapVSset.js: document features and differences between Set and Map

---
+++ js_practise/Set and Map/mapVSset.js
@@ -0,0 +1,15 @@
+/*
+| Feature           | Set            | Map                  
+| ----------------- | -------------- | -------------------- 
+| Stores            | Values         | Key-value pairs      
+| Duplicate values  | ❌ No          | Values can duplicate 
+| Duplicate keys    | Not applicable | ❌ No                 
+| Access            | `has()`        | `get()`              
+| Add               | `add()`        | `set()`              
+| Delete            | `delete()`     | `delete()`           
+| Count             | `size`         | `size`               
+| Index based       | ❌             | ❌                    
+| Keys              | Same as values | Separate keys        
+| Key can be object | N/A            | ✅                    
+| Iteration         | Values         | Key-value pairs      
+*/
