3766b889c151ea27d7e47d0f1b31a184b11ff9ac
sagarika <sagarikas@mindfiresolutions.com>
2026-09-17 00:27:24 +0530

    add pagination.js: implement documentation for pagination styles with examples

---
+++ CSS Selectors/pagination.js
@@ -0,0 +1,13 @@
+/*
+Pagination
+----------
+WEBSITE: https://www.smashingmagazine.com/category/javascript/
+
+.pagination li (anywhere inside .pagination)
+li[class="pagination__next"]
+.pagination li:nth-of-type(2)
+.pagination ul > li (direct children of .pagination ul)
+.pagination li[class="pagination__next"]
+.pagination >ul>li:nth-of-type(3)
+
+*/
