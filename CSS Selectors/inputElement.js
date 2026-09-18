0cbeb073ad3f55fe520c0021f9e85b56f717a80f
sagarika <sagarikas@mindfiresolutions.com>
2026-09-18 11:12:58 +0530

    add inputElement.js: expand documentation with additional input field selectors and examples

--- CSS Selectors/inputElement.js
+++ CSS Selectors/inputElement.js
@@ -1,1 +1,12 @@
 //input tag
+//WEBSITE USED: https://www.facebook.com/
+// input[id="_R_1h6kqsqppb6amH1_"]
+// input[type="password"] //password field //email address/mobile number field
+// input[name="email"]
+// input[name^="ema"]
+// input[name$="mail"]
+// input#_R_1h6kqsqppb6amH1_
+// div input#_R_1h6kqsqppb6amH1_
+
+
+
