47642cf8bb211fdd8bdcffdfdce32401c23b8fc5
sagarika <sagarikas@mindfiresolutions.com>
2026-09-21 11:46:28 +0530

    add class.js: create a new file to document CSS selectors for the username text box

---
+++ CSS Selectors/class.js
@@ -0,0 +1,7 @@
+//WEBSITE USED: https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+//Element: username text box
+/*.oxd-input
+.oxd-input[name="username"]
+.oxd-input[placeholder="Username"]
+.oxd-input-group div input[placeholder="Username"]
+*/
