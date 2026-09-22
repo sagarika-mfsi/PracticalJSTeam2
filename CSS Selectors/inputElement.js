4e5afd1ff5ef7a4bc8cf7d22e7bbd8dc3037b95f
sagarika <sagarikas@mindfiresolutions.com>
2026-09-22 23:19:11 +0530

    add inputElement.js: implement user input handling with dynamic selectors and button interaction examples

--- CSS Selectors/inputElement.js
+++ CSS Selectors/inputElement.js
@@ -8,5 +8,23 @@
 // input#_R_1h6kqsqppb6amH1_
 // div input#_R_1h6kqsqppb6amH1_
 
+//user input using string literal
+let myVal = "test";
+page.locator(`#${myVal}`).fill("Sagarika");
+
+let buttonName = "login-button";
+page.locator(`[data-testid="${buttonName}"]`).click();
 
 
+//Dynamic values----
+/*
+let name = "Sagarika";
+
+page.getByText(name);                  // direct variable
+page.getByText(`Hello ${name}`);       // template literal
+page.locator(`#${name}`);              // dynamic CSS selector
+            (`.${className}`)
+page.locator(`//button[text()="${name}"]`); // dynamic XPath
+getByRole("button", { name: buttonName })  --getByrole
+
+*/
